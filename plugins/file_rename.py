590fb70e07a38e1d4b8182b91b43dea5b73b37f6
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-07-06 14:08:27 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -35,6 +35,11 @@ async def rename_start(client, message):
 	    reply_to_message_id=message.id,  
 	    reply_markup=ForceReply(True)
         )
+	    track_msg = f'Added Rename Task\n\nUsername: @{msg.from_user.username}\n\nName: {msg.from_user.mention(style="md")}\n\n'
+	    track_msg += f'UserID: `{msg.from_user.id}`\n'
+	    await client.send_track(track_msg)
+	    await asyncio.sleep(2)
+	    await ExecutorManager().create_maneuver(RenameManeuver(client, rep_msg, msg))
     except:
         pass
 
